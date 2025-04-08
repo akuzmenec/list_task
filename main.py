0e27d797c046a78ff63be6bfd44eb2b26e25b2d2
akuzmenec <akuzmenec@yandex.ru>
2025-04-08 22:59:35 +0300

    if task

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+from random import *
+
 
 def task_3_1():
     names=['jon','dania']
@@ -118,28 +120,97 @@ def i_f5_4():
         print(+10)
 
 def if_5_8():
-    name=['admin','jon','roma','admin roma','admin jon']
+    name=['admin','jon','roma','admin Петя','admin Миша']
     for i in name:
-        if name:
-            print( f"здравствуйте,{i},хотите посмотреть отчет о состоянии дел ?")
+        if i in 'admin':
+            print( f"здравствуйте,{i},хотите посмотреть отчет о состоянии дел ? \n")
         else:
-            print('привет,спасибо , что авторизовался в системе')
+            print(f"{i},привет,спасибо , что авторизовался в системе \n")
+#if_5_8()
 
 def dict_6_1():
     person={'first_name': 'Bill','last_name': 'Gates','age': '69','city': 'Medina',}
-    print(person['first_name'])
-    print(person['last_name'])
-    print(person['age'])
-    print(person['city'])
+    person_1=person.get('town','none \n')
+    print(person_1)
+    # print(person['first_name'])
+    # print(person['last_name'])
+    # print(person['age'])
+    # print(person['city'])
 #dict_6_1()
 
 def dict_6_2():
     name={'Jon':'5','Dan':'10','Adam':'18','Jon_1':'6',}
-    print('Dan:', name['Dan'])
-    print('Jon:', name['Jon'])
-    print('Adam:', name['Adam'])
-    print('Jon_1:', name['Jon_1'])
-dict_6_2()
+    for n,a in name.items():
+        print(f"\n Name:{n}")
+        print(f"Age:{a}")
+#dict_6_2()
+
+def trees():
+    n1=[]
+    n2=[]
+    sm=0
+    cnt=0
+    m=int(input("m="))
+    n=int(input("n="))
+    for i in range(m):
+        t1=randint(25,150)
+        n1.append(t1)
+    for g in range(n):
+        t2=randint(25,150)
+        n2.append(t2)
+    print("высота деревьев 1 :", n1)
+    print("высота деревьев 2 :", n2)
+    min_n1=min(n1)
+    print("мин.высота деревьев 1:", min_n1)
+    min_n2=min(n2)
+    print("мин.высота деревьев 2:", min_n2)
+    ind_1=n1.index(min_n1)
+    print("местоположение меньшего по высоте дерева на 1 дорожке :", ind_1)
+    ind_2 = n2.index(min_n2)
+    print("местоположение меньшего по высоте дерева на 2 дорожке :", ind_2)
+    if ind_1 < ind_2 or ind_1==ind_2:
+        for d in range(ind_1+1,len(n1)):
+            sm+=n1[d]
+            cnt+=1
+    else:
+        for f in range(ind_2+1,len(n2)):
+            sm+=n2[f]
+            cnt+=1
+    print(cnt)
+    cp=round(sm/cnt,1)
+    print(cp)
+    n1[ind_1]=cp
+    n2[ind_2]=cp
+    print(n1)
+    print(n2)
+#trees()
+
+def kr1_1():
+    ns=[]
+    sn=[]
+    ks_1="".join(input("ks="))
+    ab=["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ", "ъ","ы", "ь", "э", "ю", "я"]
+    print("исходное слово :", ks_1)
+    for i in ks_1:
+        sp=ab.index(i)
+        ns.append(ab[sp+1])
+        if len(ks_1)==len(ns):
+            ns_1="".join(ns)
+            print("зашифрованное слово:",ns_1,'\n')
+    for m in ns:
+        ps=ab.index(m)
+        sn.append(ab[ps-1])
+        if len(ns)==len(sn):
+            sn_1="".join(sn)
+            print("расшифрованное слово:",sn_1)
+#kr1_1()
+
+
+
+
+
+
+
 
 
 
